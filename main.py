8a81bc13f61f963852fd1cc05e3959298564773b
DumbMachine <ratin.kumar.2k@gmail.com>
2020-08-01 22:54:12 +0530

    1

--- main.py
+++ main.py
@@ -7,11 +7,24 @@ import matplotlib.pyplot as plt
 from utils import *
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
-import io
-from PIL import Image
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
